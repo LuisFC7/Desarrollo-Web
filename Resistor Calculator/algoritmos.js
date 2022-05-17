3afc780c48494383af6b72ba1d31eaed5aabc0e0
Luis Antonio Flores Castro <55300309+LuisFC7@users.noreply.github.com>
2022-05-16 22:38:20 -0500

    new

--- Resistor Calculator/algoritmos.js
+++ Resistor Calculator/algoritmos.js
@@ -2,69 +2,71 @@ function valor(){
     let va1=parseInt(document.getElementById('color1').value);
     let va2=parseInt(document.getElementById('color2').value);
     let va3=parseInt(document.getElementById('color3').value);
+    var va4=document.getElementById('color4').value;
     let ty2=0;
     let result=0;
+    let fin,fin2,fin3;
 
     switch(va3){
         case 1:
             ty2=((va1*10)+va2);
-            alert("Valor de Resistencia: " +ty2+ "Ω");
+            fin="Valor de Resistencia: " +ty2+ "Ω";
         break;
 
         case 10:
             ty2=((va1*10)+va2)*10;
-            alert("Valor de Resistencia: " +ty2+ "Ω");
+            fin="Valor de Resistencia: " +ty2+ "Ω";
         break;
 
         case 100:
             ty2=(((va1*10)+va2))/10;
-            alert("Valor de Resistencia: " +ty2+ "kΩ");
+            fin="Valor de Resistencia: " +ty2+ "kΩ";
         break;
 
         case 100:
             ty2=(((va1*10)+va2))/10;
-            alert("Valor de Resistencia: " +ty2+ "kΩ");
+            fin="Valor de Resistencia: " +ty2+ "kΩ";
         break;
 
         case 1000:
             ty2=(((va1*10)+va2));
-            alert("Valor de Resistencia: " +ty2+ "kΩ");
+            fin="Valor de Resistencia: " +ty2+ "kΩ";
         break;
 
         case 10000:
             ty2=(((va1*10)+va2))*10;
-            alert("Valor de Resistencia: " +ty2+ "kΩ");
+            fin="Valor de Resistencia: " +ty2+ "kΩ";
         break;
 
         case 100000:
             ty2=(((va1*10)+va2))/10;
-            alert("Valor de Resistencia: " +ty2+ "MΩ");
+            fin="Valor de Resistencia: " +ty2+ "MΩ";
         break;
 
         case 1000000:
             ty2=(((va1*10)+va2));
-            alert("Valor de Resistencia: " +ty2+ "MΩ");
+            fin="Valor de Resistencia: " +ty2+ "MΩ";
         break;
 
         case 10000000:
             ty2=(((va1*10)+va2))*10;
-            alert("Valor de Resistencia: " +ty2+ "MΩ");
+            fin="Valor de Resistencia: " +ty2+ "MΩ";
         break;
 
 
         case 100000000:
             ty2=(((va1*10)+va2))/10;
-            alert("Valor de Resistencia: " +ty2+ "GΩ");
+            fin="Valor de Resistencia: " +ty2+ "GΩ";
         break;
 
         case 1000000000:
             ty2=(((va1*10)+va2));
             result=ty2;
-            alert("Valor de Resistencia: " +ty2+ "GΩ");
+            fin="Valor de Resistencia: " +ty2+ "GΩ";
         break;
     }
-    result=document.getElementById('prueba');
-    result.innerHTML=ty2;
-    
+    result=document.getElementById('resultado');
+    result.innerHTML=fin;
+   // let tolerancia=document.getElementById('tolerancia');
+   // tolerancia.innerHTML=va4;
 }
-
